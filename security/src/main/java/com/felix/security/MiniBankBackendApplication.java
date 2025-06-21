4b7611615fa0ad2848bb344ff8cc5950170ae484
Felix Liando <fliando1023@gmail.com>
2025-06-21 13:04:18 +0700

    First Commit

---
+++ security/src/main/java/com/felix/security/MiniBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.felix.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBankBackendApplication.class, args);
+	}
+
+}
---
+++ security/src/test/java/com/felix/security/MiniBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
